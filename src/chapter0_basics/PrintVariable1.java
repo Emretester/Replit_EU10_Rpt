a58d62f2eda04d4ac0c2fd255baf77408f343625
Emretester <emreyeni2019@gmail.com>
2023-10-09 21:11:01 +0300

    Some finished

---
+++ src/chapter0_basics/PrintVariable1.java
@@ -0,0 +1,15 @@
+package chapter0_basics;
+
+public class PrintVariable1 {
+
+    public static void main(String[] args) {
+        //DO NOT TOUCH THIS PART
+        int count = 100;
+
+        //WRITE YOUR CODE HERE
+
+        System.out.println(count);
+
+    }
+
+}
---
+++ src/chapter0_basics/PrintVariable2.java
@@ -0,0 +1,14 @@
+package chapter0_basics;
+
+public class PrintVariable2 {
+
+    public static void main(String[] args) {
+        //DO NOT TOUCH THESE LINE/LINES.
+        String message = "I am a Cydeo school student.";
+
+        //WRITE YOUR CODE BELOW THIS LINE:
+
+        System.out.println(message);
+
+    }
+}
