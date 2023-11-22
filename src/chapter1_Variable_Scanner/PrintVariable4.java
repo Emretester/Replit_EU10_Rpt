9bfdffa955d2c8faaaf0b6b0c985f576438ca3dc
Emretester <emreyeni2019@gmail.com>
2023-11-22 23:56:39 +0300

    Chapter 1 finished

---
+++ src/chapter1_Variable_Scanner/PrintVariable4.java
@@ -0,0 +1,46 @@
+package chapter1_Variable_Scanner;
+
+import java.util.Scanner;
+
+public class PrintVariable4 {
+
+    public static void main(String[] args) {
+
+        //DO NOT TOUCH THESE LINE/LINES.
+        Scanner input = new Scanner(System.in);
+        String word = input.nextLine();
+
+        //WRITE YOUR CODE BELOW:
+
+        System.out.println("Today's word of the day is: " + word);
+
+
+    }
+}
+
+/*
+
+### Given a String variable **`word`** that has some value (from Scanner), use the variable to display the word of the day.
+
+**Note:** You don't need to do anything with the Scanner.  Use the variable as done before.
+
+Main topics: print statement, variables, String, concatenation, Scanner
+
+```
+Ex:
+  Input:
+    Java
+
+  Output:
+    Today's word of the day is: Java
+```
+```
+Ex:
+  Input:
+    Apple
+
+  Output:
+    Today's word of the day is: Apple
+```
+
+ */
---
+++ src/chapter1_Variable_Scanner/Time.java
@@ -0,0 +1,52 @@
+package chapter1_Variable_Scanner;
+
+import java.util.Scanner;
+
+public class Time {
+
+    public static void main(String[] args) {
+
+        //1. WRITE YOUR CODE HERE:
+
+        int hour, minute, second;
+        String amOrPm;
+
+        //DO NOT TOUCH BELOW LINES.
+        Scanner sc = new Scanner(System.in);
+        hour = sc.nextInt();
+        minute = sc.nextInt();
+        second = sc.nextInt();
+        amOrPm = sc.next();
+        //#############################
+
+        //2. WRITE YOUR CODE HERE:     12:24:33 PM
+
+        System.out.println(hour + ":" + minute + ":" + second + " " + amOrPm);
+
+
+    }
+}
+
+/*
+
+### In this task, you will need to use multiple variables and combine them together into the time of the day.
+```
+  For example it will be something like this:
+    12:24:33 PM
+```
+
+1. In the beginning of the code, declare three `int` variables: `hour, minute, second` and declare a `String` variable: `amOrPm`.
+> You only need to declare, no assignment.
+
+2. In the middle of the code, the variables will be assigned a value using Scanner.
+> Do not touch anything in this section.
+
+3. In the end of the code, use the variables to put together and print the time of the day using the following format:
+
+  ```
+  hours:minutes:seconds amOrPm
+  ```
+
+Main topics: variables, primitive datatypes, String, Scanner, concatenation
+
+ */
